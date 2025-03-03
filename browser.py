7ea93631443432a43a3531ef05e4db15a28353ac
vexalous <vexalous@hotmail.com>
2025-03-03 15:57:02 -0500

    Add address bar.

--- browser.py
+++ browser.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 import sys
@@ -10,12 +10,20 @@ class Browser(QMainWindow):
         
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("https://duckduckgo.com/"))
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.change_browser_url)
         
         layout = QVBoxLayout()
+        layout.addWidget(self.url_bar)
         layout.addWidget(self.browser)
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+    def change_browser_url(self):
+        url = self.url_bar.text()
+        if not url.startswith("https://"):
+            url = "https://" + url
+        self.browser.setUrl(QUrl(url))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

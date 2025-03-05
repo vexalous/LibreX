ed35bcc6a373f1b84909c7685f53e29b0c79f749
vexalous <vexalous@hotmail.com>
2025-03-04 19:31:23 -0500

    Update browser.py

--- browser.py
+++ browser.py
@@ -1,6 +1,9 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QVBoxLayout, QWidget,
+    QLineEdit, QTabWidget, QPushButton
+)
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtCore import QUrl
+from PySide6.QtCore import QUrl, Qt
 from PySide6.QtGui import QIcon
 import sys
 
@@ -9,11 +12,9 @@ class Browser(QMainWindow):
         super().__init__()
         self.setWindowTitle("LibreX Web Browser")
         self.setWindowIcon(QIcon('browser/assets/icons/favicons/favicon.ico'))
-        
-        self.browser = QWebEngineView()
+
         self.default_search_engine_url = "https://duckduckgo.com"
         self.default_search_engine_search_path = "/?q="
-        self.browser.setUrl(QUrl(self.default_search_engine_url))
         
         self.url_bar = QLineEdit()
         self.url_bar.setStyleSheet("""
@@ -31,18 +32,78 @@ class Browser(QMainWindow):
         """)
         self.url_bar.returnPressed.connect(self.change_browser_url)
         
+        self.tab_widget = QTabWidget()
+        self.tab_widget.setTabsClosable(True)
+        self.tab_widget.setMovable(True)
+        self.tab_widget.tabCloseRequested.connect(self.close_current_tab)
+        self.tab_widget.currentChanged.connect(self.current_tab_changed)
+        
+        self.plus_button = QPushButton("+")
+        self.plus_button.setFixedSize(24, 24)
+        self.plus_button.clicked.connect(lambda: self.new_tab())
+        self.tab_widget.setCornerWidget(self.plus_button, Qt.TopRightCorner)
+        
+        self.new_tab()
+        
+        central_widget = QWidget()
         layout = QVBoxLayout()
         layout.addWidget(self.url_bar)
-        layout.addWidget(self.browser)
-        central_widget = QWidget()
+        layout.addWidget(self.tab_widget)
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+    
+    def new_tab(self, url=None, label="New Tab", switch=True):
+        browser = QWebEngineView()
+        if url is None:
+            url_obj = QUrl(self.default_search_engine_url)
+        else:
+            url_obj = QUrl(url)
+        browser.setUrl(url_obj)
         
+        index = self.tab_widget.addTab(browser, label)
+        if switch:
+            self.tab_widget.setCurrentIndex(index)
+        
+        browser.urlChanged.connect(lambda qurl, browser=browser: self.update_url_bar(qurl, browser))
+        browser.loadFinished.connect(lambda ok, browser=browser: self.update_tab_title(browser))
+    
+    def close_current_tab(self, index):
+        if self.tab_widget.count() > 1:
+            self.tab_widget.removeTab(index)
+    
     def change_browser_url(self):
-        url = self.url_bar.text()
-        if not url.startswith("http://") and not url.startswith("https://"):
-            url = self.default_search_engine_url + self.default_search_engine_search_path + url
-        self.browser.setUrl(QUrl(url))
+        user_input = self.url_bar.text().strip()
+        
+        if not user_input:
+            return
+        
+        if not user_input.lower().startswith("http://") and not user_input.lower().startswith("https://"):
+            if '.' in user_input:
+                user_input = "https://" + user_input
+            else:
+                user_input = self.default_search_engine_url + self.default_search_engine_search_path + user_input
+                
+        current_browser = self.tab_widget.currentWidget()
+        if current_browser:
+            current_browser.setUrl(QUrl(user_input))
+    
+    def update_url_bar(self, qurl, browser):
+        if self.tab_widget.currentWidget() == browser:
+            self.url_bar.setText(qurl.toString())
+    
+    def current_tab_changed(self, index):
+        current_browser = self.tab_widget.widget(index)
+        if current_browser:
+            self.url_bar.setText(current_browser.url().toString())
+    
+    def update_tab_title(self, browser):
+        index = self.tab_widget.indexOf(browser)
+        if index != -1:
+            title = browser.page().title()
+            if title:
+                self.tab_widget.setTabText(index, title)
+            else:
+                self.tab_widget.setTabText(index, browser.url().toString())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
